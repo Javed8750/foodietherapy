7643e6a0d68040de150801a096cceff7561e8d62
Javed <66407522+Javed8750@users.noreply.github.com>
2020-07-23 09:39:52 +0530

    preloader time decreases

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 window.onload = function () {  // make sure that whole site is loaded
     var p = document.getElementById("preloader");
     var s = document.getElementById("status");
-    var fade = setInterval(fadeOut, 3000);
+    var fade = setInterval(fadeOut, 2000);
     function fadeOut(){
         p.style.opacity = 0;
         s.style.opacity = 0;
